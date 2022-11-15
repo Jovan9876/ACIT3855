cb20bea67673d1bb81b7521661c2faf22ebb5a38
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-14 18:30:13 -0800

    react changes to show service health

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.png';
 import './App.css';
 
 import EndpointAudit from './components/EndpointAudit'
+import ServiceAudit from './components/ServiceAudit'
 import AppStats from './components/AppStats'
 
 function App() {
 
     const endpoints = ["weight", "step"]
+    const services = ["reciever", "storage", "processing", "audit"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
+    })
+
+    const running_services = services.map((service) => {
+        return <ServiceAudit key={service} service={service}/>
     })
 
     return (
@@ -19,6 +25,8 @@ function App() {
                 <AppStats/>
                 <h1>Audit Endpoints</h1>
                 {rendered_endpoints}
+                <h1>Service Health</h1>
+                {running_services}
             </div>
         </div>
     );

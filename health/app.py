7dbfd60bbed5f8c67dd020249fecea536adec61a
Ubuntu <azureuser@ACIT3855.c5vkmuoqpahuvfrce1hf13deza.bx.internal.cloudapp.net>
2022-11-18 21:02:04 +0000

    removed network host from processing health works

--- health/app.py
+++ health/app.py
@@ -2,13 +2,15 @@ import datetime
 import json
 import logging
 import logging.config
+import time
+
 
 import connexion
 import requests
 import yaml
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS, cross_origin
-
+from requests.adapters import HTTPAdapter, Retry
 with open("app_conf.yml", "r") as f:
     app_config = yaml.safe_load(f.read())
 
@@ -22,26 +24,21 @@ logger = logging.getLogger("basicLogger")
 
 def checkHealth():
     logger.info("Checking health of services")
-    max_retries = app_config["connection"]["retries"]
     statusObj = {}
     # Loop over services in config
     for service in app_config["services"]:
         logger.info(f"Checking health of {service}")
-        current_tries = 0
         # Try to get the health of the service if error its down
-        while current_tries < max_retries:
-            try:
-                res = requests.get(f"{app_config['services'][service]}/health", timeout=5)
-                if res.status_code == 200:
-                    statusObj[service] = "Running"
-                    break
-                else:
-                    statusObj[service] = "Down"
-            except requests.exceptions.ConnectionError:
-                logger.info(f"Timed out connecting to {service}")
-                current_tries += 1
-            # if service not in statusObj:
-            #     statusObj[service] = "Down"
+        try:
+            res = requests.get(f"{app_config['services'][service]}/health", timeout=5)
+            if res.status_code == 200:
+                statusObj[service] = "Running"
+            else:
+                statusObj[service] = "Down"
+        except requests.exceptions.ConnectionError as e:
+            logger.info(f"Error connecting to {service}")
+            if service not in statusObj:
+                statusObj[service] = "Something went wrong"
     # Add the last updated time to the current time
     statusObj["lastUpdate"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 

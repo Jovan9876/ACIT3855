8a8f2262d2e19aa20298a3393790b4fa215b7e15
Jovan9876 <jovansandhu3@gmail.com>
2022-11-17 19:48:23 -0800

    updated health

--- health/app.py
+++ health/app.py
@@ -22,20 +22,25 @@ logger = logging.getLogger("basicLogger")
 
 def checkHealth():
     logger.info("Checking health of services")
+    max_retries = app_config["connection"]["retries"]
     statusObj = {}
     # Loop over services in config
     for service in app_config["services"]:
         logger.info(f"Checking health of {service}")
+        current_tries = 0
         # Try to get the health of the service if error its down
-        try:
-            res = requests.get(f"{app_config['services'][service]}/health", timeout=5)
-            if res.status_code == 200:
-                statusObj[service] = "Running"
-            else:
-                statusObj[service] = "Down"
-        except requests.exceptions.ConnectionError:
-            if service not in statusObj:
-                statusObj[service] = "Down"
+        while current_tries < max_retries:
+            try:
+                res = requests.get(f"{app_config['services'][service]}/health", timeout=5)
+                if res.status_code == 200:
+                    statusObj[service] = "Running"
+                    break
+                else:
+                    statusObj[service] = "Down"
+            except requests.exceptions.ConnectionError:
+                logger.info(f"Timed out connecting to {service}")
+            # if service not in statusObj:
+            #     statusObj[service] = "Down"
     # Add the last updated time to the current time
     statusObj["lastUpdate"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 

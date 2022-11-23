df9a5ab225088037794ec96020654ebef0c0e28f
Jovan9876 <jovansandhu3@gmail.com>
2022-11-22 19:23:31 -0800

    updated create_tables_mysql for storage service

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -1,8 +1,17 @@
 import yaml
 import mysql.connector
+import os
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-with open("app_conf.yml", "r") as f:
+with open(app_conf_file, "r") as f:
     app_config = yaml.safe_load(f.read())
 
 
